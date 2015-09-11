e9f6b21e7cf2be3649eed5ef775ebce6ab7e66cf
fd <670289978@qq.com>
2015-09-11 17:46:35 +0800

    frame

---
+++ DDFrametext/UIView+frame.h
@@ -0,0 +1,17 @@
+//
+//  UIView+frame.h
+//  新浪
+//
+//  Created by tarena500 on 15/8/5.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (frame)
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+@end
